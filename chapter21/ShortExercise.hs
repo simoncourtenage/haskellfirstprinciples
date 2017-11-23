f96375b053c272605afd764a1d24ceb4d0c65dc1
Simon Courtenage <courtenage@gmail.com>
2017-11-23 10:26:15 +0000

    Change to ShortExercise.hs in Chapter 21

---
+++ chapter21/ShortExercise.hs
@@ -0,0 +1,18 @@
+module ShortExercise where
+
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = rev . cap
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap cap rev
+
+
+
